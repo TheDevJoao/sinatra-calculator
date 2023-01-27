936d4118c0b9914a087837b9936985770dfe522a
thedevjoao <thejoao94@gmail.com>
2023-01-27 10:06:39 -0300

    test: adds tests for the sinatra calculator

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,15 +1,41 @@
 require_relative '../spec_helper'
 
-RSpec.describe 'The Calculator App' do
+RSpec.describe 'App' do
   def app
     App
   end
 
-  it "should load the home page" do
+  it "loads the home page" do
     get '/'
     expect(last_response).to be_ok
   end
 
-  it "should load a welcome message" do
+  it "loads error page" do
+    get '/error'
+    expect(last_response).to include{"<h1>Error, cannot divide by zero!</h1>"}
+  end
+
+  it "checks if the form loaded" do
     get '/'
+    expect(last_response.body).to include{"<form class='calculator-container' action='/calculate' method='post'>"}
+  end
+
+  it "checks if the input fields loaded" do
+    get '/'
+    expect(last_response.body).to include{"<input required='true' type='number' step='0.01' placeholder='Number 1' name='input_one' value='<%= @result %>'/>"}
+    expect(last_response.body).to include{"<input required='true' type='number' step='0.01' placeholder='Number 2' name='input_two'/>"}
+  end
+
+  it "checks if the operator buttons loaded" do
+    get '/'
+    expect(last_response.body).to include{"<button name='operation' value='+'>+</button>"}
+    expect(last_response.body).to include{"<button name='operation' value='-'>-</button>"}
+    expect(last_response.body).to include{"<button name='operation' value='x'>x</button>"}
+    expect(last_response.body).to include{"<button name='operation' value='/'>/</button>"}
+  end
+
+  it "checks if the result input loaded" do
+    get '/'
+    expect(last_response.body).to include{"<input class='result' type='number' readonly placeholder='Result' value='<%= @result %>'>"}
+  end
 end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -1,1 +1,49 @@
 require_relative '../spec_helper'
+
+describe Calculator do
+  subject(:calculator) { Calculator.new() }
+
+  describe '#initialize' do
+    it 'initializes an instance variable called result' do
+      expect(calculator.instance_variable_get(:@result)).to eq(0)
+    end
+  end
+
+  describe '#add' do
+    it 'adds two numbers' do
+      expect(calculator.add(2, 2).result).to eq(4)
+    end
+  end
+
+  describe '#add' do
+    it 'adds two numbers' do
+      expect(calculator.add(2, 2).result).to eq(4)
+    end
+  end
+
+  describe '#subtract' do
+    it 'subtracts two numbers' do
+      expect(calculator.subtract(2, 2).result).to eq(0)
+    end
+  end
+
+  describe '#multiply' do
+    it 'multiplies two numbers' do
+      expect(calculator.multiply(3, 3).result).to eq(9)
+    end
+  end
+
+  describe '#divide' do
+    context 'when inputs are not zero' do  
+      it 'divides two numbers' do
+        expect(calculator.divide(9, 3).result).to eq(3)
+      end
+    end
+
+    context 'when an input is zero' do
+      it 'throws an error when dividing by zero' do
+        expect { calculator.divide(0,3).result }.to raise_error(ZeroDivisionError, "Cannot divide by zero!")
+      end
+    end
+  end
+end
