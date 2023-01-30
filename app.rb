7f8fb69a26d8c2ae70aa604e933b8eb49c60a6a5
thedevjoao <thejoao94@gmail.com>
2023-01-29 23:13:48 -0300

    refactor: implemented the factory design pattern and removed input validation in the frontend, implementing validations in the backend

--- app.rb
+++ app.rb
@@ -6,39 +6,11 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/error' do 
-    status 404
-    erb:error 
-  end
-
   post '/calculate' do
-    calculator = Calculator.new
-    operation = params[:operation]
-    input_one = params[:input_one].to_f
-    input_two = params[:input_two].to_f
-
-    case operation
-    when "+"
-      @result = calculator.add(input_one, input_two).result
-    when "-"
-      @result = calculator.subtract(input_one, input_two).result
-    when "x"
-      @result = calculator.multiply(input_one, input_two).result
-    when "/"
-      begin
-        if input_one == 0 || input_two == 0
-          raise ZeroDivisionError, "Cannot divide by zero!"
-        end
-        @result = calculator.divide(input_one, input_two).result
-      rescue ZeroDivisionError
-        redirect '/error'
-      end
-    end
-    if @result.round == @result
-      @result = @result.to_i
-    else
-      @result = @result.round(2)
-    end
+    input_one = params[:input_one]
+    input_two = params[:input_two]
+    @result = Calculator.get_operation(params).calculate
+    
     erb :index
   end
 end
--- models/calculator.rb
+++ models/calculator.rb
@@ -1,32 +1,62 @@
 class Calculator
-  def initialize
-    @result = 0
+
+  attr_reader :input_one, :input_two
+  
+  def initialize(params)
+    @input_one = params[:input_one].to_f
+    @input_two = params[:input_two].to_f
   end
 
-  def add(input_one, input_two)
-    @result = input_one + input_two
-    self
+  def calculate
+    raise NotImplementedError, "Method not yet implemented"
   end
-
-  def subtract(input_one, input_two)
-    @result = input_one - input_two
-    self
-  end
-
-  def multiply(input_one, input_two)
-    @result = input_one * input_two
-    self
-  end
-
-  def divide(input_one, input_two)
-    if input_one == 0 || input_two == 0
-      raise ZeroDivisionError, "Cannot divide by zero!"
+  
+  def self.get_operation params
+    operation = params[:operation]
+    case operation
+    when "+"
+      Addition.new(params)
+    when "-"
+      Subtraction.new(params)
+    when "x"
+      Multiplication.new(params)
+    when "/"
+      Division.new(params)
     end
-    @result = input_one / input_two
-    self
   end
+  
+end
 
-  def result
-    @result
+class Addition < Calculator 
+  
+  def calculate
+    input_one + input_two
   end
+  
+end
+
+class Subtraction < Calculator
+  
+  def calculate
+    input_one - input_two
+  end
+  
+end
+
+class Multiplication < Calculator
+  
+  def calculate
+  input_one * input_two
+  end
+  
+end
+
+class Division < Calculator
+  
+  def calculate
+    raise ZeroDivisionError, "Cannot divide by zero!" if input_two == 0
+  
+    input_one / input_two
+  end
+  
 end
